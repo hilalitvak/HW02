d83b3a48d3aca6be1a3bcb5b0c26e340c7e2677f
hila1 <hila.litvak@campus.technion.ac.il>
2023-06-03 15:58:13 +0300

    2

--- src/Function.java
+++ src/Function.java
@@ -1,12 +1,11 @@
 public class Function {
-    private double value;
-    private double[] coefficients;
+    private double[] values;
 
     /**
      * the constructor of the class
      */
-    public Function(double value) {
-        this.value = value;
+    public Function(double... values) {
+        this.values = values;
     }
 
     /**
@@ -16,7 +15,7 @@ public class Function {
      * @return the value of the function at x
      */
     public double valueAt(double x) {
-        return 0;
+        return this.values[0];
     }
 
     /**
@@ -32,8 +31,8 @@ public class Function {
      *
      * @return the derivative of the function
      */
-    public double derivative() {
-        return 0;
+    public Function derivative() {
+        return new Function(0);
     }
 
     /**
@@ -43,18 +42,27 @@ public class Function {
      * @return the derivation of the function in the segment between a and b
      */
     public double bisectionMethod(double a, double b) {
-        return 0;
+        return bisectionMethod(a,b, 0.00001);
     }
 
     /**
      * calculates the derivation of the function in the segment between a and b, with an epsilon error
+     * with the bisection method
      * @param a the left side of the segment
      * @param b the right side of the segment
      * @param epsilon the error
      * @return the derivation of the function in the segment between a and b
      */
     public double bisectionMethod(double a, double b, double epsilon) {
-        return 0;
+        double left= a, right = b, mid;
+        while(right - left > epsilon){
+            mid= (left + right)/2;
+            if(this.valueAt(left)*this.valueAt(right) > 0)
+                left= mid;
+            else
+                right=mid;
+        }
+        return (left + right)/2;
     }
 
     /**
@@ -64,20 +72,52 @@ public class Function {
      * @return
      */
     public double newtonRaphsonMethod(double a, double epsilon) {
-        return 0;
+        double[] x= new double[this.values.length];
+        x[0]=a;
+        int k=1;
+        while(Math.abs(this.valueAt(x[k])) < epsilon){
+            x[k+1]= x[k] - (this.valueAt(x[k])/this.derivative().valueAt(x[k]));
+            k++;
+        }
+        return x[k];
     }
 
     public double newtonRaphsonMethod(double a) {
-        return 0;
+        return newtonRaphsonMethod(a, 0.00001);
     }
+
+    /**
+     * gives the factorial of n
+     * @param n the number to factorial
+     * @return the factorial of n
+     */
+    public double factorial(int n){
+        double factorial=1;
+        for(int i=1; i <= n; i++)
+            factorial *= i;
+        return factorial;
+    }
+
 
     /**
      * gives taylorPolynomial of n power of function
      * @param n
      * @return
      */
-    public double taylorPolynomial(double n){
-        return 0;
+    public Polynomial taylorPolynomial(int n){
+        Polynomial taylor;
+        Function newFunction= this;
+        double[] coefficients= new double[n+1];
+        if(n==0)
+            taylor = new Polynomial(0);//return 0
+        else{
+            for(int i=0; i < n; i++){
+                coefficients[i]= newFunction.derivative().valueAt(0)/factorial(i);
+                newFunction= newFunction.derivative();//derivative of the function
+            }
+        }
+        taylor = new Polynomial(coefficients);
+        return taylor;
     }
 
 }
--- src/Polynomial.java
+++ src/Polynomial.java
@@ -7,7 +7,6 @@ public class Polynomial extends Function{
      * @param coefficients the coefficients of the polynomial
      */
     public Polynomial(double... coefficients) {
-        super(0);
         this.coefficients = coefficients;
     }
 
@@ -16,9 +15,12 @@ public class Polynomial extends Function{
      * @return the representation of the function as a string
      */
     @Override
-    public String toString() {
+    public String toString()
+    {
         String poly_string="";
-        for(int i=0; i < this.coefficients.length; i++){
+        if(this.coefficients[0] != 0)
+            poly_string += this.coefficients[0]+"+";//add the constant to string
+        for(int i=1; i < this.coefficients.length; i++){
             if (this.coefficients[i] != 0)
                 if(this.coefficients[i] == 1 || this.coefficients[i] == -1)
                     poly_string += "x^"+i+"+";//add the power of x
@@ -28,6 +30,33 @@ public class Polynomial extends Function{
                     poly_string += this.coefficients[i]+"x^"+i+"+";//add the coefficient and the power of x
         }
         return poly_string.substring(0, poly_string.length()-2);//remove the last "+"
+    }
+
+    /**
+     * gives the value of the function in a certain point
+     * @param x the value of the point x
+     * @return the value of the function in point x
+     */
+    public double valueAt(double x){
+        double value=0;
+        for(int i=0; i < this.coefficients.length; i++){
+            if(i==0)
+                value+= this.coefficients[i];
+            else
+                value+= this.coefficients[i]*Math.pow(x, i);
+        }
+        return value;
+    }
+
+    /**
+     * gives the derivative of the function
+     * @return the derivative of the function
+     */
+    public Function derivative(){
+        double[] derivative_coefficients= new double[this.coefficients.length-1];
+        for(int i=1; i < this.coefficients.length; i++)
+            derivative_coefficients[i-1]= this.coefficients[i]*i;
+        return new Polynomial(derivative_coefficients);
     }
 
 
